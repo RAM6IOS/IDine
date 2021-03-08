956cf18c31a357dfad15c5e99cd8eccb15d3a9cb
RAM6IOS <58077232+RAM6IOS@users.noreply.github.com>
2021-03-08 11:30:28 +0100

    Initial Commit

---
+++ iDine/iDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineApp.swift
+//  iDine
+//
+//  Created by MAC on 8/3/2021.
+//
+
+import SwiftUI
+
+@main
+struct iDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
